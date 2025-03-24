15f2de1b1defe7c9d843bfbd0d191846d576a42d
Mradul999 <demorgans007@gmail.com>
2025-03-24 18:10:28 +0530

    commit

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,50 +1,121 @@
-import { User } from "lucide-react";
-import { useNavigate } from "react-router-dom";
-import { useAuth } from "../contexts/AuthContext";
-import { ALERT_CLASSES } from "../constants";
-import { useAlert } from "../contexts/AlertContext";
+import { useState } from "react";
+import { useAuth } from "../../contexts/AuthContext";
+import { Link, useNavigate } from "react-router-dom";
+import { CentredContainer } from "../../components/auth/CentredContainer";
+import { AuthHeader } from "../../components/auth/AuthHeader";
+import { AuthBottomRedirect } from "../../components/auth/AuthBottomRedirect";
+import { useForm } from "react-hook-form";
+import { PasswordInput } from "../../components/auth/PasswordInput";
+import { useAlert } from "../../contexts/AlertContext";
+import { ALERT_CLASSES } from "../../constants";
 
-export default function NavBar() {
+export default function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
   const { showAlert } = useAlert();
-  const { currentUser, logout } = useAuth();
+  const [showErrors, setShowErrors] = useState(false);
+  const { login } = useAuth();
+  const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
 
-  async function handleLogout() {
+  async function onSubmit(data) {
     try {
-      await logout();
-      navigate("/login");
-      showAlert(ALERT_CLASSES.SUCCESS, "Successfully logged out.");
+      setLoading(true);
+      await login(data.email, data.password);
+      setLoading(false);
+      navigate("/");
     } catch {
-      showAlert(ALERT_CLASSES.ERROR, "Failed to log out.");
+      showAlert(ALERT_CLASSES.ERROR, "Failed to log in.");
+      setTimeout(() => {
+        setLoading(false);
+      }, 300);
     }
   }
 
   return (
-    <div className="navbar bg-base-200">
-      <div className="flex-1">
-        <a className="ml-2 text-xl font-medium">Personal Cloud Storage</a>
+    <CentredContainer>
+      <AuthHeader title="Login" subtitle="Hi, Welcome back 👋" />
+
+      {/* <div>
+        <button className="my-3 flex w-full items-center justify-center space-x-2 rounded-lg border border-slate-200 py-3 text-center text-slate-700 transition duration-150 hover:border-slate-400 hover:text-slate-900 hover:shadow">
+          <img
+            src="https://www.svgrepo.com/show/355037/google.svg"
+            className="h-6 w-6"
+            alt=""
+          />{" "}
+          <span>Login with Google</span>
+        </button>
       </div>
-      {currentUser && (
-        <div className="flex-none">
-          <div className="dropdown dropdown-end">
-            <div
-              tabIndex={0}
-              role="button"
-              className="avatar btn btn-circle btn-ghost"
-            >
-              <User />
-            </div>
-            <ul
-              tabIndex={0}
-              className="menu dropdown-content menu-sm z-[1] mt-3 w-52 rounded-box bg-base-100 p-2 shadow"
-            >
-              <li>
-                <a onClick={handleLogout}>Logout</a>
-              </li>
-            </ul>
-          </div>
+
+      <span className="w-full text-center text-lg text-slate-500">Or</span> */}
+
+      <form className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <label className="label">
+            <span className="label-text text-base">Email</span>
+          </label>
+          <input
+            {...register("email", {
+              required: "Email is required.",
+              pattern: {
+                value: /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/,
+                message: "Email is not valid",
+              },
+            })}
+            type="text"
+            autoComplete="email"
+            placeholder="Email Address"
+            className={
+              "input input-bordered w-full" +
+              (showErrors && errors.email ? " input-error" : "")
+            }
+          />
+          {showErrors && errors.email && (
+            <p className="text-error">{errors.email.message}</p>
+          )}
         </div>
-      )}
-    </div>
+        <div>
+          <label className="label">
+            <span className="label-text text-base">Password</span>
+          </label>
+          <PasswordInput
+            inputParams={{
+              ...register("password", {
+                required: "Password is required",
+              }),
+              placeholder: "Enter Password",
+              autoComplete: "current-password",
+            }}
+            hasError={showErrors && errors.password}
+          />
+          {showErrors && errors.password && (
+            <p className="text-error">{errors.password.message}</p>
+          )}
+        </div>
+
+        <div className="link link-primary p-1 hover:text-neutral-900">
+          <Link to="/forgot-password">Forgot Password?</Link>
+        </div>
+
+        <button
+          disabled={loading}
+          className="btn btn-primary btn-block"
+          type="submit"
+          onClick={() => setShowErrors(true)}
+        >
+          {loading && <span className="loading loading-spinner" />}
+          Login
+        </button>
+
+        <AuthBottomRedirect
+          text="Not registered yet?"
+          linkText="Register now"
+          linkTo="/signup"
+        />
+      </form>
+    </CentredContainer>
   );
 }
